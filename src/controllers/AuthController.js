03c5dfb2fe07f61d9e52b44bbf9acce404533925
Senhe Hao <senhehao@protonmail.com>
2019-11-26 23:41:20 -0600

    Finally hashed passwords like I should have.

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -2,49 +2,53 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/bungleDB', {useNewUrlParser: true});
 
 const Users = require('../models/users.js')
+const bcrypt = require("bcryptjs");
 
 module.exports = {
     async register (req, res) {
-        Users.findOne({
+        await Users.findOne({
             username: req.body.username
         }, (err, user) => {
             if(err) console.log(err)
             if(!user)
             {
-                res.send({
-                    message: `Hello ${req.body.username}, you have been registered. Not that anything has really changed.`
-                })
                 const newUser = new Users({
                     username: req.body.username,
                     email : req.body.email,
-                    password : req.body.password
+                    password : bcrypt.hashSync(req.body.password, 10)
                 })
                 newUser.save().catch(err => console.log(err));
+                res.send({
+                    message: `Hello ${req.body.username}, you have been registered. Not that anything has really changed.`
+                })
                 console.log(`success ${req.body.username} ${req.body.email}`)
             }
             else
             {
-                res.send({
-                    message: `This username ${req.body.username} has already been taken. Try another one.`
+                res.status(403).send({
+                    error: `This username has been taken.`
                 })
                 console.log(`failure`)
             }
         })
     },
     async login (req, res) {
-        Users.findOne({
-            username: req.body.username,
-            password: req.body.password
+        await Users.findOne({
+            username: req.body.username
         }, (err, user) => {
             if(err) console.log(err)
             if(!user)
             {
                 res.status(403).send({ error: 'Incorrect username or password.'})
             }
+            else if(!bcrypt.compareSync(req.body.password, user.password))
+            {
+                res.status(403).send({ error: 'Incorrect username or password.'})
+            }
             else
             {
                 res.send({
-                    message: `Welcome ${req.body.username}! The test was successful.`
+                    message: `Welcome ${req.body.username}! The test was successful.`,
                 })
                 console.log(`login successful`)
             }
